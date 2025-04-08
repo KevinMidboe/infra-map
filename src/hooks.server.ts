bd33174b4a4b206d23563d410f6cf682670ea26d
Kevin Midboe <kevin.midboe@gmail.com>
2025-04-08 23:21:09 +0200

    sadge

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,8 @@ import { dev } from '$app/environment';
 export const handle: Handle = async ({ event, resolve }) => {
 	if (dev) {
 		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+	} else {
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 	}
 
 	return await resolve(event);
