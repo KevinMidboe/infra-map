d45693bc9b94b0d79cbacc847b5a033b18e310c2
Kevin Midboe <kevin.midboe@gmail.com>
2025-08-17 22:38:16 +0200

    enables experimental remote functions

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			external: ['force-graph']
-		}
-	},
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
